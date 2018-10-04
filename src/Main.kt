6b572c13fa0ef1b4407d82672f53030881b84d0f
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-04 23:50:09 +0900

    new create

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+  // String, Char
+  val msg: String = "hello world"
+  val c: Char = 'a'
+  println(msg)
+  println(c)
+
+  // Byte, Short, Int, Long
+  val i: Int = 100
+  val l: Long = 55555555555L
+  println(i)
+  println(l)
+
+  // Float, Double
+  val d: Double = 234.523
+  val f: Float = 12.344F
+  println(d)
+  println(f)
+
+  // Boolean (true/ false)
+  val flag: Boolean = true
+  println(flag)
+}
