89a00a1fe72021e1e3aac6bb65a0bf9fbe044ee6
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-07 15:39:48 +0900

    fix(Main.kt): Main.kt fix
    https://dotinstall.com/lessons/basic_kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,37 @@
-// data class
+// Collection
+/*
+ - List (Immutable/Mutable)
+ - Set (Immutable/Mutable)
+ - Map (Immutable/Mutable)
+*/
 
-data class Point(val x: Int, val y: Int)
+fun test(): List<String> {
+  val temp = listOf("1", "2", "3")
+  return temp
+}
 
 fun main(args: Array<String>) {
-  val p1 = Point(3, 5)
-//  val p2 = Point(3, 5)
-  val p2 = p1.copy()
+  val sales1 = listOf(20, 30, 40)
+  val sales2 = mutableListOf(20, 30, 40)
 
-  println(p1)
-  println(p2)
-  println(if (p1 == p2) "same" else "not same")
+  println(sales1)
+  println(sales2)
 
-  val (x, y) = p1
-  println("$x:$y")
+  sales2[1] = 50
+
+  println(sales1)
+  println(sales2)
+
+  println(sales1.size)
+
+  for (sale in sales1) {
+    println(sale)
+  }
+
+  for (sale in sales2) {
+    println(sale)
+  }
+
+  println(test())
+
 }
