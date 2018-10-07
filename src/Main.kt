bb14757655123154015f2a9eadaf6ff015eada76
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-07 15:24:46 +0900

    fix(Main.kt): Main.kt fix
    https://dotinstall.com/lessons/basic_kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,16 @@
-// generics
+// data class
 
-class MyInteger {
-  fun getThree(x: Int) {
-    println(x)
-    println(x)
-    println(x)
-  }
-}
-
-class MyData<T> {
-  fun getThree(x: T) {
-    println(x)
-    println(x)
-    println(x)
-  }
-}
+data class Point(val x: Int, val y: Int)
 
 fun main(args: Array<String>) {
-  val mi = MyInteger()
-  mi.getThree(55)
+  val p1 = Point(3, 5)
+//  val p2 = Point(3, 5)
+  val p2 = p1.copy()
 
-  println()
+  println(p1)
+  println(p2)
+  println(if (p1 == p2) "same" else "not same")
 
-  val i = MyData<Int>()
-  i.getThree(32)
-  val s = MyData<String>()
-  s.getThree("hello")
+  val (x, y) = p1
+  println("$x:$y")
 }
