e221a59da863ac9545dd1b9c94c794daecab7b93
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-11 01:14:37 +0900

    電卓テスト

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,39 @@
 // null
 
+private var nStr: String = ""
+private val nList = ArrayList<Long>()
+private val oList = ArrayList<Char>()
+
 fun main(args: Array<String>) {
-//  val s: String = null
-  val s: String? = null
-//  println(s)
 
-//  if (s != null) {
-//    println(s.length)
-//  } else {
-//    println(null)
-//  }
+  
 
-//  println(s?.length) // 上記と同様の処理
-//  println(s?.length ?: -1) // null以外の表示を行いたいとき
-  println(s!!.length) // nullだったときExceptionを投げる
+}
+
+fun calculator(): Long {
+  var i = 0
+  val nList = ArrayList<Long>(nList)
+  val oList = ArrayList<Char>(oList)
+  nList.add(nStr.toLong())
+  try {
+    while (i < oList.size) {
+      if (oList[i] == '×' || oList[i] == '÷') {
+        val result = if (oList[i] == '×') nList[i] * nList[i + 1] else nList[i] / nList[i + 1]
+        nList[i] = result
+        nList.removeAt(i + 1)
+        oList.removeAt(i)
+        i--
+      } else if (oList[i] == '−') {
+        oList[i] = '+'
+        nList[i + 1] = nList[i + 1] * -1
+      }
+      i++
+    }
+  } catch (e: Exception) {
+    println(e)
+  }
+
+  val result = nList.sum()
+
+  return (result * 1.08).toLong()
 }
