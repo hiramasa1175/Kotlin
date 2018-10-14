e6abedeb7b14adc2476a9d933f402c657e37b47c
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-14 15:25:20 +0900

    電卓テスト中

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,19 @@
-fun main(args: Array<String>) {
 
+import java.text.NumberFormat
+
+fun main(args: Array<String>) {
+  val str = "1,000+1,000"
+  val value = toStringWithNotSeparator(str)
+  println(value.toStringWithSeparator())
 }
 
 fun Int.toStringWithSeparator() = "%,d".format(this)
+
+fun toStringWithNotSeparator(str: String): Int {
+  val n : Int
+
+  val number = NumberFormat.getInstance().parse(str)
+  n = number.toInt()
+
+  return n
+}
