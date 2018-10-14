90683a9195e440bbb5b72d8eb7dbd3151bf9a610
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-15 00:47:05 +0900

    文字列と追加する文字を渡すとカンマ付きで返す関数

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,24 @@
 import java.lang.StringBuilder
 
-var test = "1000+2000+10000"
+var test = "100+200000+10000"
 
 fun main(args: Array<String>) {
-  println(toStringWithSeparator("1000"))
+  println(toStringWithSeparator(test, "1"))
 }
 
 fun toStringWithSeparator(str: String, appe: String = ""): String {
   var result = str + appe
   result = result.replace(",", "")
-  var strBuil = StringBuilder().append(result)
+  var strBuilder = StringBuilder().append(result)
   var count = 0
-  for ((index, value) in strBuil.withIndex().reversed()) {
+  for ((index, value) in strBuilder.withIndex().reversed()) {
     if (value in '0'..'9') count++ else count = 0
-    if (count == 3) {
-      strBuil = strBuil.insert(index, ",")
+    if (count == 3 && index != 0) {
+      if (strBuilder[index - 1] in '0'..'9')
+        strBuilder = strBuilder.insert(index, ",")
       count = 0
     }
   }
 
-  return strBuil.toString()
+  return strBuilder.toString()
 }
