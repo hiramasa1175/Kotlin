9a33ccbef1c53ea57fa6d04faf9af0867e34348e
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-19 23:19:15 +0900

    電卓テスト

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,14 @@
 import java.lang.StringBuilder
+import java.math.BigDecimal
 
-var test = "100+200000+10000"
+var test = 11_000
 
 fun main(args: Array<String>) {
-  println(toStringWithSeparator(test, "1"))
-}
+  for (off in 1..99) {
+    val off2 = 0.01 * off
+    val off3 = (1.0 - off2)
+    val result = (test * off3).toFloat()
 
-fun toStringWithSeparator(str: String, appe: String = ""): String {
-  var result = str + appe
-  result = result.replace(",", "")
-  var strBuilder = StringBuilder().append(result)
-  var count = 0
-  for ((index, value) in strBuilder.withIndex().reversed()) {
-    if (value in '0'..'9') count++ else count = 0
-    if (count == 3 && index != 0) {
-      if (strBuilder[index - 1] in '0'..'9')
-        strBuilder = strBuilder.insert(index, ",")
-      count = 0
-    }
+    println(result.toInt())
   }
-
-  return strBuilder.toString()
 }
